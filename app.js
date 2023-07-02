ec86f6bd5f37263156310ee9efb9516db206d347
mahmoud <mahmoudwardany18@gmail.com>
2023-07-02 09:10:39 +0300

    update

--- app.js
+++ app.js
@@ -7,12 +7,26 @@ import connectDB from './config/DB.js'
 import ApiError from './utils/apiError.js'
 import { globalHandlerError } from './middlewares/error.middleware.js'
 import morgan from 'morgan'
+import hpp from 'hpp'
+import {cors} from 'cors'
+import { rateLimit } from 'express-rate-limit'
+import xxs from 'xss-clean'
 //DB
 connectDB()
 //MiddleWare
 app.use(express.json())
 app.use(morgan('dev'))
-//baseUrl
+//Security
+app.use(cors())
+const limiter = rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	max: 200, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+})
+// Apply the rate limiting middleware to all requests
+app.use(limiter)
+app.use(xxs())
+app.use(hpp())
+//BaseUrl
 const baseUrl=process.env.BASR_URL
 const port=process.env.PORT || 7000
 //router
